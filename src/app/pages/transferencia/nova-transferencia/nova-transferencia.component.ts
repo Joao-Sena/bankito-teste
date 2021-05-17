dd1de344ce328963e1de888d44cccc6bfa5d1c03
Joao-Sena <joaosena.contato@gmail.com>
2021-05-17 12:14:25 -0300

    Criação de mascara para o campo input de dinheiro

--- src/app/pages/transferencia/nova-transferencia/nova-transferencia.component.ts
+++ src/app/pages/transferencia/nova-transferencia/nova-transferencia.component.ts
@@ -15,6 +15,26 @@ export class NovaTransferenciaComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  moneyMask() {
+
+    this.valorTransferencia = this.valorTransferencia.replace(",", "");
+
+    if(this.valorTransferencia.length <= 3) {
+      this.valorTransferencia = `0,${this.valorTransferencia.slice(-2)}`;
+    }
+
+    if(this.valorTransferencia.length > 4) {
+      if(this.valorTransferencia[0] == '0') { this.valorTransferencia = this.valorTransferencia.replace('0', '')}
+
+      this.valorTransferencia = `${this.valorTransferencia.slice(0, this.valorTransferencia.length -2)},${this.valorTransferencia.slice(-2)}`;
+    }
+
+    if(this.valorTransferencia.length === 4 && !this.valorTransferencia.includes(',')) {
+      this.valorTransferencia = `${this.valorTransferencia.slice(0,2)},${this.valorTransferencia.slice(-2)}`;
+    }
+
+  }
+
   // Não foi possível obter os dados da conta ao digitar o número da conta, pois só tem API pra obter através do ID não do Número
   // Então essa página ficou sem funcionalidade no quesito de pegar dados do Mock
 
